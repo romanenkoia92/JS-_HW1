859d8dc213f825405dc0aeb9ac54f31f3709ba7b
romanenkoia92 <romanenkoia92@gmail.com>
2023-01-18 21:50:16 +0200

    change

--- HW1_JS.js
+++ HW1_JS.js
@@ -1,24 +1,24 @@
-// let item_1 = 5;
-// console.log(item_1)
+let item_1 = 5;
+console.log(item_1)
 
-// let item_2 = 3;
-// console.log(item_2)
+let item_2 = 3;
+console.log(item_2)
 
-// let item_3 = item_1 + item_2;
-// console.log(item_3)
+let item_3 = item_1 + item_2;
+console.log(item_3)
 
-// let item_4 = "Yolochka";
-// console.log(item_4)
+let item_4 = "Yolochka";
+console.log(item_4)
 
-// let item_5 = item_3;
-// let item_6 = 15;
-// let item_6_type = typeof item_6;
+let item_5 = item_3;
+let item_6 = 15;
+let item_6_type = typeof item_6;
 
-// console.log("item_6 ==", item_6, "item_6_type ==", item_6_type);
+console.log("item_6 ==", item_6, "item_6_type ==", item_6_type);
 
-// let item_7 = "item_6";
-// let item_7_type = typeof(item_7);
-// console.log("item_7 == "+item_7, "item_7_type == "+ item_7_type)
+let item_7 = "item_6";
+let item_7_type = typeof(item_7);
+console.log("item_7 == "+item_7, "item_7_type == "+ item_7_type)
 
 let age_1 = 10;
 let age_2 = 18;
